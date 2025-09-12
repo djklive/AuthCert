d95d49b4a49bdcee595de81167b0100dcec6c576
djklive <franckdjk@gmail.com>
2025-09-12 05:22:32 +0100

    chore: Remove unused test-route.js file and clean up Vercel configuration by eliminating the functions section

--- src/test-route.js
+++
@@ -1,5 +0,0 @@
-// Test simple pour vérifier que la route fonctionne
-console.log('Test de la route /verifier-certificat');
-console.log('URL actuelle:', window.location.href);
-console.log('Pathname:', window.location.pathname);
-console.log('Search params:', window.location.search);
